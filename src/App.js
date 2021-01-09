4bd41331d031cad368e34a8733d4c87143abc460
devrohanb <dev.rohanb@gmail.com>
2021-01-09 23:18:59 +0530

    After implementing React Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      {/* // TODO: Navbar */}
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      {/* TODO: Menu component */}
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/* // TODO: Navbar */}
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        {/* TODO: Menu component */}
+        <Menu dishes={this.state.dishes} /> {/* Here "dishes" is a prop */}
+      </div>
+    );
+  }
 }
 
 export default App;
